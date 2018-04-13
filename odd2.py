a9fd374f3725645d4b23689e0aeeb446bf59512a
Ben Kohli <dashsoul92@gmail.com>
2018-04-12 20:16:09 -0600

    Completed the second example from chapter 1.

---
+++ odd2.py
@@ -0,0 +1,18 @@
+#! c:\[C:\Python35]\python.exe
+
+from datetime import datetime
+from time import sleep
+from random import randint
+
+odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
+	    21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+	    41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+
+for i in range(5):
+	right_this_minute = datetime.today().minute
+
+	if right_this_minute in odds:
+		print("This minute, which is " + str(right_this_minute) + ", seems a little odd.")
+	else:
+		print(str(right_this_minute) + " isn't an odd minute.")
+	sleep(randint(1, 60))
